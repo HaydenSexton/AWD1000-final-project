ef40381d67141f2f91e4a6d16cf28afa57793b85
HaydenSexton <Hayden_Sexton@insideranken.org>
2025-04-26 04:54:21 -0500

    commit vite config fix

--- Lumon-employee-list/vite.config.js
+++ Lumon-employee-list/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/Lumon-employee-list/',
+  base: '/AWD1000-final-project/',
+  plugins: [react()]
 })
